ff2802a7ff12a63225b6855a9e9deb992ab7bdaa
Lukas Grossar <lukas.grossar@adfinis-sygroup.ch>
2016-12-08 18:05:29 +0100

    fix mixed indentation in entrypoint.sh
    And add .editorconfig to avoid future issues.

--- galera-basic-centos/entrypoint.sh
+++ galera-basic-centos/entrypoint.sh
@@ -42,16 +42,16 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
 		echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
 		echo "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
-  fi
+	fi
 	if [ "$MYSQL_USER" -a ! "$MYSQL_PASSWORD" ]; then
 		echo "CREATE USER '$MYSQL_USER'@'%'  ;"         >> "$tempSqlFile"
 		echo "CREATE USER '$MYSQL_USER'@'localhost'  ;" >> "$tempSqlFile"
-  fi
+	fi
 
-  if [ "$MYSQL_USER" -a  "$MYSQL_DATABASE"  ]; then
-  	echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
-  	echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'localhost' ;" >> "$tempSqlFile"
-  fi
+	if [ "$MYSQL_USER" -a  "$MYSQL_DATABASE"  ]; then
+		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
+		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'localhost' ;" >> "$tempSqlFile"
+	fi
 
 	echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
 
