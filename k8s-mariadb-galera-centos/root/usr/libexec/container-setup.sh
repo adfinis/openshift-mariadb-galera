587d3b45353a5fbe5dc80b080cfd0d2a4da22ebe
Cyrillvw <cyrill.vonwattenwyl@adfinis-sygroup.ch>
2017-08-15 13:23:57 +0200

    Move mysqld-socket from /var/lib/mysql to /var/run/mysql For supporting glusterfs PVs. Also updated image version and added subPath for PV mounts

--- k8s-mariadb-galera-centos/root/usr/libexec/container-setup.sh
+++ k8s-mariadb-galera-centos/root/usr/libexec/container-setup.sh
@@ -8,7 +8,7 @@
 # Locations
 MYSQL_CONFIG_DIR="/etc/my.cnf.d"
 MYSQL_DATA_DIR="/var/lib/mysql"
-
+MYSQL_RUN_DIR="/var/run/mysql"
 
 # Fix data directory permissions
 rm -rf ${MYSQL_DATA_DIR}
@@ -23,3 +23,9 @@ mkdir -p ${MYSQL_CONFIG_DIR}
 chown -R 27:0 ${MYSQL_CONFIG_DIR}
 chmod -R g+w ${MYSQL_CONFIG_DIR}
 restorecon -R ${MYSQL_CONFIG_DIR}
+
+# Create run directory
+mkdir -p ${MYSQL_RUN_DIR}
+chown -R 27:0 ${MYSQL_RUN_DIR}
+chmod -R g+w ${MYSQL_RUN_DIR}
+restorecon -R ${MYSQL_RUN_DIR}
